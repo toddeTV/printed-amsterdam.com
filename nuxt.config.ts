cb35bb145cb10159da96daded73a64981301eab6
Thorsten Seyschab <business@todde.tv>
2025-05-07 17:05:12 +0200

    feat: add NuxtUI to nuxt with TailwindCSS and a main CSS file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,11 +2,16 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
 
+  css: [
+    '~/assets/css/main.css',
+  ],
+
   devtools: {
     enabled: true,
   },
 
   modules: [
     '@nuxt/eslint',
+    '@nuxt/ui',
   ],
 })
