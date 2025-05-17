bec3f11db79d3939fcf7afa7381d2bbb913683f9
Thorsten Seyschab <business@todde.tv>
2025-05-17 15:42:14 +0200

    feat: extend schema for collections

--- content.config.ts
+++ content.config.ts
@@ -15,8 +15,9 @@ export default defineContentConfig({
       type: 'data',
       source: 'stages/**/*.json',
       schema: z.object({
+        slug: z.string().describe('The slug of the stage. This is used to identify and link the stage to other collections.'),
         name: z.string(),
-        place: z.string().optional().describe('An optional string indicating the location (building/room/address).'),
+        place: z.string().optional().describe('An optional string indicating the location (building/room/address/etc.).'),
       }),
     }),
 
@@ -24,10 +25,12 @@ export default defineContentConfig({
       type: 'data',
       source: 'speakers/**/*.json',
       schema: z.object({
+        slug: z.string().describe('The slug of the speaker. This is used to identify and link the speaker to other collections. Use `firstname-lastname` as convention.'),
+
         name: z.string(),
         description: z.string(),
         biography: z.string(),
-        image: z.string(),
+        image: z.string().editor({ input: 'media' }).describe('The image of the speaker.'),
 
         company: z.string().optional(),
 
@@ -43,9 +46,17 @@ export default defineContentConfig({
       type: 'data',
       source: 'talks/**/*.json',
       schema: z.object({
+        type: z.enum(['talk', 'panel', 'workshop']).describe('The type of the talk.'),
         title: z.string(),
         description: z.string(),
-        speakers: z.array(z.string()),
+        speakers: z.array(z.string()).default([]).describe('An array of speaker slugs.'),
+        dateTime: z.date().describe('The date and time of the talk when it takes place.'),
+        stage: z.string().describe('The slug of the stage where the talk takes place.'),
+        resources: z.array(z.object({
+          url: z.string().url().describe('Put in the full URL to the resource.'),
+          description: z.string().optional().describe('An optional description for the link.'),
+          icon: z.string().optional().editor({ input: 'icon' }).describe('Optionally override the icon if it is not correctly detected.'),
+        })).default([]),
       }),
     }),
   },
