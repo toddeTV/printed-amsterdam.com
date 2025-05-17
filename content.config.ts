0d048302605718944d3e7a9056a7352def080830
Thorsten Seyschab <business@todde.tv>
2025-05-17 23:37:56 +0200

    feat: add content collection for index landing page

--- content.config.ts
+++ content.config.ts
@@ -4,6 +4,13 @@ export default defineContentConfig({
   collections: {
     // -------- static pages
 
+    index: defineCollection({
+      type: 'page',
+      source: '0.index.yml',
+      schema: z.object({
+      }),
+    }),
+
     pages: defineCollection({
       type: 'page',
       source: 'pages/**/*.md',
@@ -15,7 +22,7 @@ export default defineContentConfig({
       type: 'data',
       source: 'stages/**/*.yml',
       schema: z.object({
-        slug: z.string().describe('The UNIQUE slug of the stage. This is used to identify and link the stage to other collections.'),
+        slug: z.string().describe('The UNIQUE slug of the stage. This is used to identify and link the stage to other collections. Never change this!'),
         name: z.string().describe('The name of the stage.'),
         place: z.string().optional().describe('An optional string indicating the location (building/room/address/etc.).'),
       }),
@@ -25,7 +32,7 @@ export default defineContentConfig({
       type: 'page',
       source: 'speakers/**/*.md',
       schema: z.object({
-        slug: z.string().describe('The UNIQUE slug of the speaker. This is used to identify and link the speaker to other collections. Use `firstname-lastname` as convention.'),
+        slug: z.string().describe('The UNIQUE slug of the speaker. This is used to identify and link the speaker to other collections. Use `firstname-lastname` as convention. Never change this!'),
         name: z.string().describe('The name of the speaker.'),
         description: z.string().describe('A short description of the speaker - one line, best only a few words!'),
         // biography: z.string().describe('A biography of the speaker. This is shown on the speaker page.'), // this is the body of the markdown file itself
@@ -34,7 +41,7 @@ export default defineContentConfig({
         socialMedia: z.array(z.object({
           url: z.string().url().describe('Put in the full URL to the account/channel/etc.'),
           description: z.string().optional().describe('An optional description for the link.'),
-          icon: z.string().optional().editor({ input: 'icon' }).describe('Optionally override the icon if it is not correctly detected.'),
+          icon: z.string().optional().editor({ input: 'icon' }).describe('Optionally override the icon. Per default it is detected automatically. Please only use `Simple Icons`'),
         })).default([]),
       }),
     }),
@@ -43,7 +50,7 @@ export default defineContentConfig({
       type: 'page',
       source: 'talks/**/*.md',
       schema: z.object({
-        slug: z.string().describe('The UNIQUE slug of the talk. This is used to identify and link the stage to other collections.'),
+        slug: z.string().describe('The UNIQUE slug of the talk. This is used to identify and link the stage to other collections. Never change this!'),
         type: z.enum(['talk', 'panel', 'workshop']).describe('The type of the talk.'),
         title: z.string().describe('The title of the talk.'),
         // abstract: z.string().describe('The abstract of the talk.'), // this is the body of the markdown file itself
@@ -53,7 +60,7 @@ export default defineContentConfig({
         resources: z.array(z.object({
           url: z.string().url().describe('Put in the full URL to the resource.'),
           description: z.string().optional().describe('An optional description for the link.'),
-          icon: z.string().optional().editor({ input: 'icon' }).describe('Optionally override the icon if it is not correctly detected.'),
+          icon: z.string().optional().editor({ input: 'icon' }).describe('Optionally override the icon. Per default it is detected automatically. Please only use `Simple Icons`'),
         })).default([]),
       }),
     }),
