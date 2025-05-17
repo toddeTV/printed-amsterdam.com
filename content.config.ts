183af488e27f792ff5876b0cf81b7c9b354b441c
Thorsten Seyschab <business@todde.tv>
2025-05-17 16:50:13 +0200

    feat: add descriptions to all content schema fields

--- content.config.ts
+++ content.config.ts
@@ -15,8 +15,8 @@ export default defineContentConfig({
       type: 'data',
       source: 'stages/**/*.json',
       schema: z.object({
-        slug: z.string().describe('The slug of the stage. This is used to identify and link the stage to other collections.'),
-        name: z.string(),
+        slug: z.string().describe('The UNIQUE slug of the stage. This is used to identify and link the stage to other collections.'),
+        name: z.string().describe('The name of the stage.'),
         place: z.string().optional().describe('An optional string indicating the location (building/room/address/etc.).'),
       }),
     }),
@@ -25,14 +25,14 @@ export default defineContentConfig({
       type: 'data',
       source: 'speakers/**/*.json',
       schema: z.object({
-        slug: z.string().describe('The slug of the speaker. This is used to identify and link the speaker to other collections. Use `firstname-lastname` as convention.'),
+        slug: z.string().describe('The UNIQUE slug of the speaker. This is used to identify and link the speaker to other collections. Use `firstname-lastname` as convention.'),
 
-        name: z.string(),
-        description: z.string(),
-        biography: z.string(),
+        name: z.string().describe('The name of the speaker.'),
+        description: z.string().describe('A short description of the speaker - one line, best only a few words!'),
+        biography: z.string().describe('A biography of the speaker. This is shown on the speaker page.'),
         image: z.string().editor({ input: 'media' }).describe('The image of the speaker.'),
 
-        company: z.string().optional(),
+        company: z.string().optional().describe('The name of the company the speaker works for.'),
 
         socialMedia: z.array(z.object({
           url: z.string().url().describe('Put in the full URL to the account/channel/etc.'),
@@ -46,9 +46,10 @@ export default defineContentConfig({
       type: 'data',
       source: 'talks/**/*.json',
       schema: z.object({
+        slug: z.string().describe('The UNIQUE slug of the talk. This is used to identify and link the stage to other collections.'),
         type: z.enum(['talk', 'panel', 'workshop']).describe('The type of the talk.'),
-        title: z.string(),
-        description: z.string(),
+        title: z.string().describe('The title of the talk.'),
+        description: z.string().describe('The description of the talk.'),
         speakers: z.array(z.string()).default([]).describe('An array of speaker slugs.'),
         dateTime: z.date().describe('The date and time of the talk when it takes place.'),
         stage: z.string().describe('The slug of the stage where the talk takes place.'),
