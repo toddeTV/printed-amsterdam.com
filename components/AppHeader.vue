53d00bd52bcb73a4dfd8ee4301dfcde1ab61feaf
Thorsten Seyschab <business@todde.tv>
2025-05-17 23:39:21 +0200

    feat: add header component

---
+++ components/AppHeader.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+  description: string
+}>()
+</script>
+
+<template>
+  <div>
+    <div class="flex flex-col lg:flex-row lg:items-center lg:justify-between gap-4">
+      <h1 class="text-3xl sm:text-4xl text-pretty font-bold text-highlighted">
+        {{ title }}
+      </h1>
+    </div>
+    <div class="text-lg text-pretty text-muted mt-4">
+      {{ description }}
+    </div>
+  </div>
+</template>
